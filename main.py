733021b152763bb450618a8a0befaea655c83b9d
Rene Potocnik <rene.potocnik1@gmail.com>
2022-08-27 11:33:09 +0200

    Get the image of a letter

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import pathlib
+import random
+
 from PIL import Image
 
 
@@ -6,9 +9,16 @@ def create_paper():
     return Image.new("RGBA", (2480, 3508), color="white")
 
 
+def get_letter_image(letter: str) -> Image:
+    pic_amount: int = 4  # How many pictures of each letter there is
+    pic: int = random.randint(1, pic_amount)
+    image: Image = Image.open(f"{pathlib.Path(__file__).parent}\\Characters\\letters\\{letter.upper()}\\{pic}.png")
+    image.show()
+
+
 def main():
     paper = create_paper()
-    paper.show()
+    # paper.show()
 
 
 if __name__ == '__main__':
