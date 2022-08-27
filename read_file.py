9e396e7bb068a58c186493a2bcf32d95f76ac6b6
Rene Potocnik <rene.potocnik1@gmail.com>
2022-08-27 11:45:36 +0200

    Rename function, add docstrings

--- read_file.py
+++ read_file.py
@@ -3,7 +3,7 @@ from tkinter import filedialog
 from typing import List
 
 
-def get_file():
+def get_file() -> str:
     """
     Open file explorer, wait for user to open a `.txt` file
 
@@ -21,7 +21,7 @@ def get_file():
 
 def read_file(file_path: str) -> List[str]:
     """
-    Read the file on the entered `file_path`, return all lines
+    Read the file on the entered `file_path`, return a list of all the paragraphs
 
     :param file_path: Location of `.txt` file
     :return: List of read lines
@@ -31,14 +31,19 @@ def read_file(file_path: str) -> List[str]:
     return contents
 
 
-def get_lines():
+def get_file_contents() -> List[str]:
+    """
+    Prompt user to select a file, read and return the list of paragraphs inside
+
+    :return: List of paragraphs in the selected file
+    """
     file_path: str = get_file()
     contents: List[str] = read_file(file_path)
     return contents
 
 
 if __name__ == '__main__':
-    text = get_lines()
-    if "y" in input("Print lines? [y/n]\n"
+    text = get_file_contents()
+    if "y" in input(f"Print {len(text)} lines? [y/n]\n"
                     "> "):
         print(text)
