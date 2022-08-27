3c072a2f239b52067b993d5983cb6e9907e8461a
Rene Potocnik <rene.potocnik1@gmail.com>
2022-08-27 22:22:20 +0200

    Get letter from file and get a random letter image

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
 import pathlib
 import random
+from typing import List, Tuple
 
 from PIL import Image
 
+import read_file
 
-def create_paper():
-    # Create a blank, white sheet of paper
-    return Image.new("RGBA", (2480, 3508), color="white")
+
+def create_paper(size: Tuple[int, int] = (2480, 3508)):
+    return Image.new("RGBA", size, color="white")
 
 
 def get_letter_image(letter: str) -> Image:
-    pic_amount: int = 4  # How many pictures of each letter there is
-    pic: int = random.randint(1, pic_amount)
+    pic_amount: int = 4  # Amount of pictures of each letter
+    # There's `pic_amount * 2` pictures of each letter in each folder
+    # THe first `pic_amount` images are for uppercase and the last `pic_amount` images are for lowercase letters
+    if letter.isupper():
+        pic: int = random.randint(1, pic_amount)
+    else:
+        pic: int = random.randint(pic_amount + 1, pic_amount * 2)
     image: Image = Image.open(f"{pathlib.Path(__file__).parent}\\Characters\\letters\\{letter.upper()}\\{pic}.png")
     image.show()
 
 
 def main():
     paper = create_paper()
-    # paper.show()
+    paper.show()
+
+    contents: List[str] = read_file.get_file_contents()
+    for paragraph in contents:
+        for letter in paragraph:
+            get_letter_image(letter=letter)
 
 
 if __name__ == '__main__':
