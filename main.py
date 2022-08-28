3879d801b987cba9a530c36159b96b34fc00d5a9
Rene Potocnik <rene.potocnik1@gmail.com>
2022-08-28 10:15:47 +0200

    Get images of all chars

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import pathlib
 import random
-from typing import List, Tuple
+from typing import List, Tuple, Dict
 
 from PIL import Image
 
 import read_file
+
+
+misc_chars: Dict[str, str] = {
+                ".": "dot",
+                "?": "question",
+                '"': "quote",
+                "/": "slash"
+            }
 
 
 def create_paper(size: Tuple[int, int] = (2480, 3508)):
@@ -15,29 +23,43 @@ def get_char_image(char: str) -> Image:
     pic_amount: int = 4  # Amount of pictures of each char
     # There's `pic_amount * 2` pictures of each char in each folder
     # The first `pic_amount` images are for uppercase and the last `pic_amount` images are for lowercase letters
-    for _ in range(1):
+    try:
+        for _ in range(1):
 
-        # Check if character is a number
-        if char.isdigit():
-            break
+            # Check if character is a number
+            if char.isdigit():
+                pic: int = random.randint(1, pic_amount)
+                path: str = f"{pathlib.Path(__file__).parent}\\Characters\\numbers\\{char}\\{pic}.png"
+                break
 
-        # Check if character is an uppercase letter
-        if char.isalpha():
-            pic: int = random.randint(1, pic_amount) if char.isupper() else \
-                random.randint(pic_amount + 1, pic_amount * 2)
-            path: str = f"{pathlib.Path(__file__).parent}\\Characters\\letters\\{char.upper()}\\{pic}.png"
-            break
-    else:
-        print(f"'{char}' was not found.")
-        return
+            # Check if character is a letter
+            if char.isalpha():
+                pic: int = random.randint(1, pic_amount) if char.isupper() else \
+                    random.randint(pic_amount + 1, pic_amount * 2)
+                path: str = f"{pathlib.Path(__file__).parent}\\Characters\\letters\\{char.upper()}\\{pic}.png"
+                break
 
-    return Image.open(path)
+            # Check if character is one of the `misc` chars
+            if char in "!,-":
+                pic: int = random.randint(1, pic_amount)
+                path: str = f"{pathlib.Path(__file__).parent}\\Characters\\misc\\{char}\\{pic}.png"
+                break
+            if char in '.?"/':
+                pic: int = random.randint(1, pic_amount)
+                path: str = f"{pathlib.Path(__file__).parent}\\Characters\\misc\\{char}\\{pic}.png"
+                break
+        else:
+            print(f"Unknown character: '{char}'")
+            return
+        return Image.open(path)
+
+    except FileNotFoundError:
+        print(f"'{char}' file not found.")
 
 
 def main():
     paper = create_paper()
     paper.show()
-
     contents: List[str] = read_file.get_file_contents()
     for paragraph in contents:
         for char in paragraph:
